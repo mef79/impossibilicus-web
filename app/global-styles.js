1b73461f738557774ee5b1765eae0c4020defcdf
Phillip Dodson <clevergadget@gmail.com>
2017-04-05 18:51:16 -0400

    moving css into global styles

--- app/global-styles.js
+++ app/global-styles.js
@@ -27,4 +27,116 @@ injectGlobal`
     font-family: Georgia, Times, 'Times New Roman', serif;
     line-height: 1.5em;
   }
+
+  #bottom {
+  height: 100px;
+  background: #dbe8fc;
+  font-size: 16px;
+  padding: 10px;
+}
+
+#undo,
+#redo {
+  margin-left: 5px;
+  margin-right: 5px;
+  float: right;
+  display: inline-block;
+  cursor: pointer;
+}
+
+#undo.disabled,
+#redo.disabled {
+  cursor: not-allowed;
+  color: #ccc;
+}
+
+#add-node {
+  display: inline-block;
+}
+
+body {
+  font: 13px sans-serif;
+  position: relative;
+  width: 800px;
+  height: 500px;
+}
+
+form {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+}
+
+.node {
+  fill: #ccc;
+  stroke: #000;
+  stroke-width: 1.5px;
+  cursor: pointer;
+}
+
+.node_selected {
+  fill: #ff7f0e;
+}
+
+.link {
+  stroke: #000;
+  stroke-width: 2;
+  cursor: pointer;
+}
+
+.link_selected {
+  stroke: #ff7f0e;
+}
+
+.tooltip {
+  position: fixed;
+  line-height: 1;
+  font-weight: bold;
+  padding: 9px;
+  background: #F5F5F5;
+  border: 1px solid #AFAFAF;
+  border-radius: 5px;
+  width: 200px;
+}
+
+.tooltip:after,
+.tooltip:before {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    color: transparent;
+}
+
+/* this border color controls the color of the triangle (what looks like the fill of the triangle) */
+.tooltip:after {
+    top: 37px;
+    left: 101px;
+    border-top: 10px solid #F5F5F5;
+    border-left: 10px solid transparent;
+    border-right:10px solid transparent;
+}
+
+/* this border color controls the outside, thin border */
+.tooltip:before {
+    top: 37px;
+    left: 100px;
+    border-top: 11px solid #858585;
+    border-left: 11px solid transparent;
+    border-right:11px solid transparent;
+}
+
+.button {
+    display: inline-block;
+    background: white;
+    float: right;
+    padding: 1px 2px 3px 2px;
+    border: 1px solid #858585;
+    border-radius: 2px;
+    cursor: pointer;
+    margin-left: 3px;
+    margin-right: 3px;
+}
 `;
