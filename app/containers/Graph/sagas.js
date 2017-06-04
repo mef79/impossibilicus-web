4db7499ba0f483c86f4e69d92753e6abdd67c487
Phillip Dodson <clevergadget@gmail.com>
2017-06-03 23:43:37 -0400

    Graph updating on select

--- app/containers/Graph/sagas.js
+++ app/containers/Graph/sagas.js
@@ -1,32 +1,34 @@
 // import { take, call, put, select } from 'redux-saga/effects'
 import { SET_SELECTED_NODE, SET_SELECTED_LINK } from './constants'
 import { put, select, takeEvery } from 'redux-saga/effects'
-import { setSelectedNode, setSelectedLink } from './actions'
+import { setSelectedNode, setSelectedLink, setShouldRedraw } from './actions'
 import { getSelectedNodeId, getSelectedLinkId } from './selectors'
 // Individual exports for testing
 
-export function* clearSelectedLink() {
+export function* redrawAndClearSelectedLink() {
   const selectedLinkId = yield select(getSelectedLinkId())
   const selectedNodeId = yield select(getSelectedNodeId())
   if (selectedLinkId && selectedNodeId) {
     yield put(setSelectedLink(null))
   }
+  yield put(setShouldRedraw(true))
 }
 
-export function* clearSelectedNode() {
+export function* redrawAndClearSelectedNode() {
   const selectedNodeId = yield select(getSelectedNodeId())
   const selectedLinkId = yield select(getSelectedLinkId())
   if (selectedNodeId && selectedLinkId) {
     yield put(setSelectedNode(null))
   }
+  yield put(setShouldRedraw(true))
 }
 
 export function* rootSaga() {
   // clear the selected link when selecting a node
-  yield takeEvery(SET_SELECTED_NODE, clearSelectedLink)
+  yield takeEvery(SET_SELECTED_NODE, redrawAndClearSelectedLink)
 
   // clear the selected node when selecting a link
-  yield takeEvery(SET_SELECTED_LINK, clearSelectedNode)
+  yield takeEvery(SET_SELECTED_LINK, redrawAndClearSelectedNode)
 }
 
 // All sagas to be loaded
