e633943bcca076bc669f3d5866ae27aea761fd48
Meaghan <mefenelon@gmail.com>
2017-07-30 14:45:03 -0400

    Add 'component renders' base test to NavigationBar

---
+++ app/utils/test-utils.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+export function mockFormStore(initialState = {}) {
+  const store = createStore((state = initialState) => {
+    return Object.assign({}, state)
+  })
+
+  return store
+}
+
+export function mountInProvider(component) {
+  return (
+    <Provider store={mockFormStore()}>
+      { component }
+    </Provider>)
+}
